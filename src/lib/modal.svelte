92066dae27d7f67cbb2b55bc84a8594d70a97393
BarasaJnr <isaacbarasa714@gmail.com>
2024-08-27 10:21:01 +0300

    init

---
+++ src/lib/modal.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import { fade, fly } from 'svelte/transition'
+
+    const dispatch = createEventDispatcher();
+</script>
+
+<div class="modal-bg" transition:fade>
+    <div class="modal" transition:fly={{ y: -100 }}>
+        <button on:click={() => {
+            dispatch('close');
+        }}>
+        Close
+        </button>
+        <slot />
+    </div>
+</div>
---
+++ src/lib/question.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { score } from './store.js'
+  
+    export let question
+    export let nextQuestion
+  
+    let isCorrect
+    let isAnswered = false
+  
+    let answers = question.incorrect_answers.map(answer => {
+      return {
+        answer,
+        correct: false,
+      }
+    })
+    let allAnswers = [
+      ...answers,
+      {
+        answer: question.correct_answer,
+        correct: true,
+      },
+    ]
+    shuffle(allAnswers)
+    function shuffle(array) {
+      array.sort(() => Math.random() - 0.5)
+    }
+  
+    function checkQuestion(correct) {
+      isAnswered = true
+      isCorrect = correct
+      if (correct) score.update(currentValue => currentValue + 1)
+    }
+  </script>
+  
+  <h3>
+    {@html question.question}
+  </h3>
+  
+  {#if isAnswered}
+    <h4 class:isCorrect>
+      {#if isCorrect}Correct 🎉{:else}Wrong 😬{/if}
+    </h4>
+  {/if}
+  
+  {#each allAnswers as answer}
+    <button
+      disabled={isAnswered}
+      on:click={() => checkQuestion(answer.correct)}
+    >
+      {@html answer.answer}
+    </button>
+  {/each}
+  
+  {#if isAnswered}
+    <div>
+      <button on:click={nextQuestion}>Next Question</button>
+    </div>
+  {/if}
+  
+  <style>
+    h4 {
+      color: red;
+    }
+    h4.isCorrect {
+      color: green;
+    }
+  </style>
